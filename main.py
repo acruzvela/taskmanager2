70b413dc25efc4d6f692c088b0231d5e1c27c9d2
acruzvela <acruzvela@hotmail.com>
2026-07-21 21:11:59 +0100

    main salvado

---
+++ main.py
@@ -0,0 +1,54 @@
+from task_manager import TaskManager
+
+def print_menu(): 
+    print("\n--- Gestor de Tareas Inteligente ---")
+    print("1. Añadir tarea")
+    print("2. Añadir tarea compleja (con IA)")
+    print("3. Listar tareas")
+    print("4. Completar tarea")
+    print("5. Eliminar tarea")
+    print("6. Salir")
+
+def main():
+
+    manager = TaskManager()
+
+    while True:
+
+        print_menu()
+
+        try:
+
+            choice = int(input("Elige una opción: "))
+
+            match choice:
+                case 1:
+                    description = input("Descripción de la tarea: ")
+                    manager.add_task(description)             
+
+                case 2:
+                    pass
+
+                case 3:
+                    manager.list_tasks()
+
+                case 4:
+                    id = int(input("ID de la tarea a completar: "))
+                    manager.complete_task(id)
+
+                case 5:
+                    id = int(input("ID de la tarea a eliminar: "))
+                    manager.delete_task(id)
+
+                case 6:
+                    print("Saliendo...")
+                    break
+                case _:
+                    print("Opción no válida. Selecciona otra.")
+
+        except ValueError:
+            print("Opción no válida. Selecciona otra.")
+
+if __name__ == "__main__":
+    main()
+    
